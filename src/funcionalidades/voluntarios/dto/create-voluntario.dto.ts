2838a6aa6b51c0da9e621e9d8aa44eda31605751
Odilon Garcez <odilon.garcez.moraes@gmail.com>
2021-09-14 22:14:05 -0300

    feat: implementa mensagens padrão de validação no dto

--- src/funcionalidades/voluntarios/dto/create-voluntario.dto.ts
+++ src/funcionalidades/voluntarios/dto/create-voluntario.dto.ts
@@ -1,10 +1,12 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { validacaoMensagem } from 'src/comuns/mensagens/mensagem-validacao';
 
 export class CreateVoluntarioDto {
   @IsString()
+  @IsNotEmpty({ message: validacaoMensagem('nome').NAO_VAZIO })
   nome: string;
 
-  @IsNotEmpty()
+  @IsNotEmpty({ message: validacaoMensagem('cpf').NAO_VAZIO })
   @IsString()
   cpf: string;
 }
